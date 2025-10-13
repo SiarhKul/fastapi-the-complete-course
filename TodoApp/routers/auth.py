2b3044eb5eb66ad613a5a4a0c248fde7fdeb17c4
Siarhey Kulazhanka <skulazhanka@exadel.com>
2025-10-13 17:06:21 +0200

    chore: User table

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,8 +1,30 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+
+from models import Users
 
 router = APIRouter()
 
-@router.get('/auth/')
-async def get_user():
-    return {'user', 'auth'}
 
+class CreateUserRequest(BaseModel):
+    email = str
+    username = str
+    first_name = str
+    last_name = str
+    role = str
+    password = str
+
+
+@router.get('/auth')
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=create_user_request.password,
+        is_active=True
+    )
+
+    return create_user_model

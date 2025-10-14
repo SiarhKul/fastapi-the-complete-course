5839814f5346066bf26b5efe09f343ccd25bcf67
Siarhey Kulazhanka <skulazhanka@exadel.com>
2025-10-14 09:35:24 +0200

    chore: Auth

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -16,10 +16,10 @@ class Users(Base):
 
 class Todos(Base):
     __tablename__ = "todos"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
     compted = Column(Boolean, default=False)
-    owner_id = Column(Integer, ForeignKey('user.id'))
+    owner_id = Column(Integer, ForeignKey('users.id'))

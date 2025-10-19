81ecd0e4d72de679948457e186e83005ef6c2fc4
Siarhey Kulazhanka <skulazhanka@exadel.com>
2025-10-19 13:20:29 +0200

    chore: protect put by id todo

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -66,12 +66,16 @@ async def create_todo(user: user_dependency, db: db_dependency, todo_request: To
     db.commit()
 
 
-@router.put('/todo/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
-async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+@router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(user: user_dependency, db: db_dependency,
+                      todo_request: TodoRequest,
+                      todo_id: int = Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
 
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get('id')).first()
     if todo_model is None:
-        raise HTTPException(status_code=404, detail='Todo not found')
+        raise HTTPException(status_code=404, detail='Todo not found.')
 
     todo_model.title = todo_request.title
     todo_model.description = todo_request.description
@@ -79,8 +83,8 @@ async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int
     todo_model.compted = todo_request.compted
 
     db.add(todo_model)
-
     db.commit()
+
 
 
 @router.delete('/todo/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)

48a1172bbf5eb2829e9356257a8275aafcfd2088
Siarhey Kulazhanka <skulazhanka@exadel.com>
2025-10-19 10:27:03 +0200

    chore: refactoring

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -15,7 +15,10 @@ from database import SessionLocal
 from starlette import status
 from jose import jwt, JWTError
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 SECRET_KEY='12345'
@@ -30,7 +33,7 @@ def get_db():
         db.close()
 
 db_dependency:type[Session] = Annotated[Session, Depends(get_db)]
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 class CreateUserRequest(BaseModel):
     email: str
@@ -45,7 +48,7 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
-@router.post('/auth', status_code=status.HTTP_201_CREATED)
+@router.post('/', status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
@@ -69,7 +72,7 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     user = authenticate_user(form_data.username, form_data.password, db)
 
     if not user:
-        return "User not found"
+      raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user")
 
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
 

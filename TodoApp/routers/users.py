dc411ed101ac4a3194f835633b97dc992e32843e
Siarhey Kulazhanka <skulazhanka@exadel.com>
2025-10-25 09:55:47 +0200

    chore: Get current user

---
+++ TodoApp/routers/users.py
@@ -0,0 +1,33 @@
+from typing import Annotated
+
+from fastapi import APIRouter, HTTPException
+from fastapi.params import Depends, Path
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import SessionLocal
+from models import Todos
+from .auth import get_current_user
+
+
+router = APIRouter(tags=['user'])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
+@router.get("/user", status_code=status.HTTP_200_OK)
+async def get_user( user:user_dependency):
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+

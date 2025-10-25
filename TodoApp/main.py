7c07f967f545678841b56a95060893a1f8cf3b31
Siarhey Kulazhanka <skulazhanka@exadel.com>
2025-10-25 09:36:48 +0200

    chore: Delete.Adming

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 import models
 from database import engine
-from routers import todos, auth
+from routers import todos, auth, admin
 
 app = FastAPI()
 
@@ -10,3 +10,4 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(admin.router)
---
+++ TodoApp/routers/admin.py
@@ -0,0 +1,48 @@
+from typing import Annotated
+
+from fastapi import APIRouter, HTTPException
+from fastapi import Depends
+from fastapi.params import Path
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import SessionLocal
+from models import Todos
+from .auth import get_current_user
+
+router = APIRouter(prefix='/admin', tags=['admin'])
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+SECRET_KEY = '12345'
+ALGORITHM = "HS256"
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
+@router.get('/todo', status_code=status.HTTP_200_OK)
+async def read_all(user: user_dependency, db: db_dependency):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Auth failed')
+    return db.query(Todos).all()
+
+@router.delete('/todo/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(user: user_dependency, db: db_dependency, todo_id:int = Path(gt=0)):
+    if user is None or user.get('user_role') !='admin':
+        raise HTTPException(status_code=401, detail="Auth failed")
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).delete()
+
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Toto not found")
+    db.commit()
+

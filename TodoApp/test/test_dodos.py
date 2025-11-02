a1b18cf832e0036720c32d40139fb6b44d6a82eb
Siarhey Kulazhanka <skulazhanka@exadel.com>
2025-11-02 10:28:08 +0100

    chore: Not found

--- TodoApp/test/test_dodos.py
+++ TodoApp/test/test_dodos.py
@@ -57,7 +57,16 @@ def test_todo():
 
 
 
-def test_read_all_autthnticated(test_todo):
+def test_read_all_authenticated(test_todo):
     response = client.get('/')
     assert response.status_code == status.HTTP_200_OK
     assert response.json() ==[ {'priority': 5, 'id': 1, 'owner_id': 1, 'title': 'Learn to code', 'description': 'Need to learn', 'compted': False}]
+
+def test_read_one_authenticated(test_todo):
+    response = client.get('/todo/1')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() =={'priority': 5, 'id': 1, 'owner_id': 1, 'title': 'Learn to code', 'description': 'Need to learn', 'compted': False}
+
+def test_read_one_authenticated_not_found(test_todo):
+    response = client.get('/todo/999')
+    assert response.status_code == status.HTTP_404_NOT_FOUND

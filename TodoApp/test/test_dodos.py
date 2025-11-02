2d161116559e4447ed67174cfcb80b0f8f6545dd
Siarhey Kulazhanka <skulazhanka@exadel.com>
2025-11-02 10:53:09 +0100

    chore: Test todos

--- TodoApp/test/test_dodos.py
+++ TodoApp/test/test_dodos.py
@@ -121,3 +121,7 @@ def test_update_todo(test_todo):
 def test_delete_todo(test_todo):
     response = client.delete("/todo/1")
     assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id ==1).first()
+    assert model is None
